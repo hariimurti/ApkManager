1189941ac26cf0d061231aafad355262f2e9abc0
Harimurti Wibowo <hariimurti@gmail.com>
2021-11-14 18:27:35 +0700

    v2021.11.14.1827

--- ApkManager/Properties/AssemblyInfo.cs
+++ ApkManager/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2021.4.13.1335")]
-[assembly: AssemblyFileVersion("2021.4.13.1335")]
+[assembly: AssemblyVersion("2021.11.14.1827")]
+[assembly: AssemblyFileVersion("2021.11.14.1827")]
